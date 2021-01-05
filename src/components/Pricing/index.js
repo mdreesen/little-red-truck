defe06da5a30bf01f25bc26e5de0f8436e9ce779
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-05 00:41:30 -0600

    changed up cards

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -11,17 +11,16 @@ function Price() {
         <div className="row"></div>
             <div className="card-deck">
             <div className="row">
-            <div className="card col-md-3">
+            <div className="card col-md-5 mt-3">
                     <img className="card-img-top" src={engagementPic} alt="Card image cap" />
                     <div className="card-body">
                     <h5 className="card-title">engagements</h5>
                     <p className="card-text">Begins at $350</p>
-                    <p className="card-text">One to Two outfit changes</p>
-                    <p className="card-text">45-60 min session</p>
+                    <p className="card-text">45-60 min session - One to two outfit changes</p>
                     <p className="card-text">20-25 edited photos, digital images & online gallery</p>
                     </div>
                 </div>
-                <div className="card col-md-3">
+                <div className="card col-md-5 mt-3">
                     <img className="card-img-top" src="..." alt="Card image cap" />
                     <div className="card-body">
                     <h5 className="card-title">Wedding</h5>
@@ -29,23 +28,27 @@ function Price() {
                     <p className="card-text">4-6 hours of coverage</p>
                     </div>
                 </div>
-                <div className="card col-md-3">
-                    <img className="card-img-top" src={birthPic} alt="Card image cap" />
-                    <div className="card-body">
-                    <h5 className="card-title">Birth</h5>
-                    <p className="card-text">$950</p>
-                    <p className="card-text">4-6 hours of coverage</p>
-                    </div>
-                </div>
-                <div className="card col-md-3">
-                        <img className="card-img-top" src={familyPic} alt="Card image cap" />
+            </div>
+                <div className="row">
+                    <div className="card col-md-5 mt-3">
+                        <img className="card-img-top" src={birthPic} alt="Card image cap" />
                         <div className="card-body">
-                        <h5 className="card-title">Family</h5>
-                        <p className="card-text">$350</p>
-                        <p className="card-text">20 - 25 edited photos, digital images & online gallery</p>
+                        <h5 className="card-title">Birth</h5>
+                        <p className="card-text">$950</p>
+                        <p className="card-text">4-6 hours of coverage</p>
                         </div>
                     </div>
-                    <div className="card col-md-3">
+                    <div className="card col-md-5 mt-3">
+                            <img className="card-img-top" src={familyPic} alt="Card image cap" />
+                            <div className="card-body">
+                            <h5 className="card-title">Family</h5>
+                            <p className="card-text">$350</p>
+                            <p className="card-text">20 - 25 edited photos, digital images & online gallery</p>
+                            </div>
+                        </div>
+                </div>
+                <div className="row">
+                <div className="card col-md-5 mt-3">
                         <img className="card-img-top" src={fresh48} alt="Card image cap" />
                         <div className="card-body">
                         <h5 className="card-title">Fresh 48</h5>
@@ -53,7 +56,7 @@ function Price() {
                         <p className="card-text">10-15 digital images & online gallery</p>
                         </div>
                     </div>
-            </div>
+                </div>
             </div>
         </section>
 
