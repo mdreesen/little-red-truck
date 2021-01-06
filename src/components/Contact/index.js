168d7e349016085249304faa56000362e9fb0cf6
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-06 16:28:40 -0600

    changed the contact page to have more information

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,9 +13,16 @@ function Contact() {
         <div className="card-contact text-center">
             <div className="card-body">
                 <div className="icon-container">
-                    <a href={icon.instagram}><i className="iconPic fab fa-instagram fa-4x" /></a>
-                    <a href={icon.facebook}><i className="iconPic fab fa-facebook-square fa-4x" /></a>
-                    <a href="mailto: stephaniemichelle01@yahoo.com"><i className="iconPic fas fa-envelope-open-text fa-4x" /></a>
+
+                    <div className="social-container">
+                        <h3>Find littleredtruckphotography on facebook and instagram</h3>
+                        <a href={icon.instagram}><i className="iconPic fab fa-instagram fa-4x" /></a>
+                        <a href={icon.facebook}><i className="iconPic fab fa-facebook-square fa-4x" /></a>
+                    </div>
+                    <div className="email-container">
+                        <h3>Email LRT Photography</h3>
+                        <a href="mailto: stephaniemichelle01@yahoo.com"><i className="iconPic fas fa-envelope-open-text fa-4x" /></a>
+                    </div>
                 </div>
             </div>
         </div>
