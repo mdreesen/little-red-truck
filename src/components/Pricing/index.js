f0ee8ebdeb44668cdf53968c56b0f44c2c89e548
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-06 16:01:45 -0600

    changed the pricing page pictures around

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import weddingPic from '../../images/ringPic.jpg';
-import fresh48 from '../../images/elliot.jpg';
+// import fresh48 from '../../images/elliot.jpg';
 import engagementPic from '../../images/familyPic5.jpg';
 import familyPic from '../../images/familyPricing.jpg';
 import birthPic from '../../images/birthPic.jpg';
@@ -33,14 +33,7 @@ function Price() {
                 </div>
             </div>
                 <div className="row">
-                    <div className="card col-md-5 mt-3">
-                        <img className="card-img-top" src={birthPic} alt="Card image cap" />
-                        <div className="card-body">
-                        <h5 className="card-title">Birth</h5>
-                        <p className="card-text">$950</p>
-                        <p className="card-text">Includes coverage of birth & online gallery</p>
-                        </div>
-                    </div>
+                    
                     <div className="card col-md-5 mt-3">
                             <img className="card-img-top" src={familyPic} alt="Card image cap" />
                             <div className="card-body">
@@ -49,8 +42,18 @@ function Price() {
                             <p className="card-text">20 - 25 edited photos, digital images & online gallery</p>
                             </div>
                         </div>
+
                 </div>
                 <div className="row">
+                <div className="card col-md-5 mt-3">
+                        <img className="card-img-top" src={birthPic} alt="Card image cap" />
+                        <div className="card-body">
+                        <h5 className="card-title">Birth</h5>
+                        <p className="card-text">$950</p>
+                        <p className="card-text">Includes coverage of birth & online gallery</p>
+                        </div>
+                    </div>
+                
                 <div className="card col-md-5 mt-3">
                         <img className="card-img-top" src={fresh48Two} alt="Card image cap" />
                         <div className="card-body">
@@ -59,6 +62,7 @@ function Price() {
                         <p className="card-text">10-15 digital images & online gallery</p>
                         </div>
                     </div>
+
                 </div>
             </div>
             </div>
